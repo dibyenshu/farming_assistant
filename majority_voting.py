4ee6627275de94057b5a85414fda95f863a120a1
Dibyanshu Mishra <dibyenshu@gmail.com>
2020-05-02 22:08:24 +0530

    fixed locationDensity count in majority voting

--- majority_voting.py
+++ majority_voting.py
@@ -1,6 +1,7 @@
 import table_users as users
 import table_user_crops as user_crops
 import table_crop_properties as crop_properties
+import MySQLdb
 
 def HumidityInRange(requiredRange,actualVal):
     minVal = list(requiredRange.split('-'))[0]
@@ -26,9 +27,21 @@ def RainfallInRange(requiredRange,actualVal):
         val = 1 
     return val
 
-def LocationScore(locationDensity):
+def LocationScore(crop):
+    locationDensity = 0
+    #get location density for the crop from the table
+    db = MySQLdb.connect(host='localhost',user='root',passwd='password',db='farming_assistant')
+    query = ' select * from user_crops;'
+    db.query(query)
+    rows = db.store_result().fetch_row(maxrows=0,how=1)
+    for row in rows:
+        if row[crop] == 1:
+            locationDensity = locationDensity + 1
+
+    #calculate the score and return
     val = (float(locationDensity)*0.3)**2
     val = -val
+    print(crop + ' ' + str(locationDensity) + '\n')
     return val
 
 def CalculatePoints(crop_points,crop_prop,currentCondition):
@@ -39,7 +52,7 @@ def CalculatePoints(crop_points,crop_prop,currentCondition):
         crop_points[crop] += temperature
         rainfall = RainfallInRange(crop_prop[crop][crop_properties.HUMIDITY],currentCondition[crop_properties.HUMIDITY])
         crop_points[crop] += rainfall
-        locationDesnsityScore = LocationScore(currentCondition['locationDensity'])
+        locationDesnsityScore = LocationScore(crop)
         crop_points[crop] += locationDesnsityScore
 
     return crop_points
