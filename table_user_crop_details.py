0be6d39cd84286ac3943baf0f0454b8a3f15e1ab
Dibyanshu Mishra <dibyenshu@gmail.com>
2020-05-10 03:46:01 +0530

    table details for storing per user crop details

---
+++ table_user_crop_details.py
@@ -0,0 +1,9 @@
+#column names of individual tables for per user
+CROPNAME = 'cropname'
+PRECIPITATION = 'precipitation'
+TEMP = 'temp'
+HUMIDITY = 'humidity'
+MOISTURE = 'moisture'
+SOIL_TEMP = 'soil_temp'
+PH = 'ph'
+WATER_PH = 'water_ph'
